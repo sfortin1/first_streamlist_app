dc1882412062830aa3eef6021ba0b207e78a907a
sfortin1 <60408862+sfortin1@users.noreply.github.com>
2023-11-28 14:01:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlist.text("Omega 3 & Blueberry Oatmeal")
+streamlist.text("Kale, Spinach & Rocket Smoothie")
+streamlist.text("Hard-Boiled Free-Range Egg")
